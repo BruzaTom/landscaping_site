b2381f4ea82d39878bef9f73d86773655b45fe06
tomux <thomasagomez77@gmail.com>
2025-10-26 20:19:44 -0500

    fixed page numbers

--- gallerygen.py
+++ gallerygen.py
@@ -2,11 +2,15 @@ import os
 
 folder = "static/images/landscaping"
 max_images = 6
+page_range = (len(os.listdir(folder)) + max_images - 1) // max_images
+
 def gen_head():
     nav = '<nav class="navbar"><ul class="link-list"><li><a href="contact.html" class="abbreviate">Contact</a></li><li><a href="index.html">Home</a></li><li><a href="payments.html">Payments</a></li></ul></nav>'
     head = f'<!DOCTYPE html><html><head><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1"><title> Gallery </title><link href="static/index.css" rel="stylesheet"></head><body><article><div><h1>Gallery</h1>{nav}<div class="pagination">'
-    for i in range(len(os.listdir(folder))//max_images):
-        head += f'<a href="gallery{i}.html">{i}</a>'
+    i = 0
+    while i < page_range - 1:  # ensures last batch 
+        head += f'<a href="gallery{i}.html">{i+1}</a>'
+        i += 1
     head += '</div><div class="gallery">'
     return head
 tail = '</div></div></article></body></html>' 
